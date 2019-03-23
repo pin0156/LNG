30821cd632aae2c3e5076ce0762456f953966eb2
pin0156@naver.com <pin0156@naver.com>
2019-03-23 15:12:44 +0900

    add download data.sh

--- script/build_lotto_dic.sh
+++ script/build_lotto_dic.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+echo "download data"
+sh download_data.sh
+
+echo ""
+echo "build lotto dic"
 python build_lotto_dic.py < ../data/lotto_data.json > ../data/lotto_statistics.json
---
+++ script/download_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get update data
+last_lotto_num_in_file=`tail -1 ../data/lotto_data.json | python -c 'import json; import sys; line=sys.stdin.readline(); data=json.loads(line); print data["drwNo"]'`
+start_lotto_num=$(($last_lotto_num_in_file + 1));
+
+date=`date "+%Y.%m.%d"`
+end_y=`date "+%Y"`
+end_m=`date "+%m"`
+end_d=`date "+%d"`
+end_lotto_num=`python -c 'import sys; from datetime import date; d=date(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])) - date(2002,12,07); print d.days/7' $end_y $end_m $end_d`
+
+echo "last lotto number in lotto_data file : $last_lotto_num_in_file"
+echo "last lotto number (today: $date): $end_lotto_num"
+if [ $last_lotto_num_in_file -eq $end_lotto_num ]; then
+	echo "Do not update"
+	exit
+fi
+
+outfile=../data/lotto_data.json
+echo "download lotto date: $start_lotto_num ~ $end_lotto_num"
+echo "update lotto data file: $outfile"
+for i in `seq $start_lotto_num $end_lotto_num`
+do
+	wget --no-check-certificate -O- "https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=$i" >> $outfile
+	echo "" >> $outfile
+done
