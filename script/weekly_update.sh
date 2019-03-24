3e6cbd49dcba2208d148a56527ee22f56ad2ef85
pin0156@naver.com <pin0156@naver.com>
2019-03-24 22:02:13 +0900

    modified weekly_update.sh

--- script/weekly_update.sh
+++ script/weekly_update.sh
@@ -4,3 +4,6 @@ HOME_PATH="/home/ec2-user/LNG"
 cd "$HOME_PATH/script"
 pwd
 sh build_lotto_dic.sh
+cp ../data/lotto_statistics.json ../www/data/lotto_statistics.json
+echo "call api: LNG_reload_dic"
+curl localhost/LNG_reload_dic
